ac144b8902d50ed990987661171def1db758fa40
Aaru1801 <1aaravbhargava@gmail.com>
2026-01-30 20:13:45 -0500

    Changed the Atticus Terminal stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "main.h"
 #include "lemlib/api.hpp" // IWYU pragma: keep
 #include "lemlib/chassis/chassis.hpp"
-#include "pros/optical.hpp" // Added include for optical sensor
+
 ASSET(right_safe_path_txt);   // name = file name with . replaced by _
 extern lemlib::Chassis chassis;
 
@@ -9,70 +9,64 @@ extern lemlib::Chassis chassis;
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 // motor groups
-// LEFT DRIVE = ports 1, 2, 3 (ALL BLUE)
-pros::MotorGroup leftMotors({1, 2, 3}, pros::MotorGearset::blue);
+// LEFT DRIVE = ports 1, 2, 19 (ALL BLUE)
+pros::MotorGroup leftMotors({-8, -9, -10}, pros::MotorGearset::blue);
 
-// RIGHT DRIVE = ports 4,5,6 (ALL BLUE)
-pros::MotorGroup rightMotors({-4, -5, -6}, pros::MotorGearset::blue);
+// RIGHT DRIVE = ports 4,5,10 (ALL BLUE)
+pros::MotorGroup rightMotors({4, 5, 6}, pros::MotorGearset::blue);
 
 // INTAKE = port 7
-pros::Motor intake(7, pros::MotorGearset::blue);
+pros::Motor intake(1, pros::MotorGearset::blue);
 
 // OUTTAKE = port 8
-pros::Motor outtake(8, pros::MotorGearset::blue);
+pros::Motor outtake(11, pros::MotorGearset::blue);
 
 // Inertial Sensor on port 10
-pros::Imu imu(10);
-// Optical sensor on port 9 (change port if needed)
-pros::Optical optical(11);
+pros::Imu imu(13);
 
 // tracking wheels
-// horizontal tracking wheel encoder. Rotation sensor, port 20, not reversed
-pros::Rotation horizontalEnc(20);
-// vertical tracking wheel encoder. Rotation sensor, port 11, reversed
-pros::Rotation verticalEnc(-11);
-// horizontal tracking wheel. 2.75" diameter, 5.75" offset, back of the robot (negative)
-lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_2, 0);
+// vertical tracking wheel encoder. Rotation sensor, port 12, reversed
+pros::Rotation verticalEnc(-12);
 // vertical tracking wheel. 2.75" diameter, 2.5" offset, left of the robot (negative)
 lemlib::TrackingWheel vertical(&verticalEnc, lemlib::Omniwheel::NEW_2, 0);
 
 // drivetrain settings
 lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
                               &rightMotors, // right motor group
-                              10, // 10 inch track width
+                              15, // 15 inch track width
                               lemlib::Omniwheel::NEW_275, // using new 2.75" omni
                               600, // drivetrain rpm is 600
-                              2 // horizontal drift is 2. If we had traction wheels, it would have been 8
+                              5 // horizontal drift is 5 cuz of 2 traction wheels. If we had all traction wheels, it would have been 8
 );
 
 // lateral motion controller
 lemlib::ControllerSettings linearController(10, // proportional gain (kP)
                                             0, // integral gain (kI)
-                                            3, // derivative gain (kD)
+                                            5, // derivative gain (kD)
                                             0, // anti windup
                                             0, // small error range, in inches
                                             0, // small error range timeout, in milliseconds
                                             0, // large error range, in inches
                                             0, // large error range timeout, in milliseconds
-                                            0 // maximum acceleration (slew)
+                                            10 // maximum acceleration (slew)
 );
 
 // angular motion controller
 lemlib::ControllerSettings angularController(2, // proportional gain (kP)
                                              0, // integral gain (kI)
-                                             10, // derivative gain (kD)
+                                             13, // derivative gain (kD)
                                              0, // anti windup
                                              0, // small error range, in degrees
                                              0, // small error range timeout, in milliseconds
                                              0, // large error range, in degrees
                                              0, // large error range timeout, in milliseconds
-                                             0 // maximum acceleration (slew)
+                                             10 // maximum acceleration (slew)
 );
 
 // sensors for odometry
 lemlib::OdomSensors sensors(&vertical, // vertical tracking wheel
                             nullptr, // vertical tracking wheel 2, set to nullptr as we don't have a second one
-                            &horizontal, // horizontal tracking wheel
+                            nullptr, // horizontal tracking wheel
                             nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
                             &imu // inertial sensor
 );
@@ -102,10 +96,6 @@ void initialize() {
     pros::lcd::initialize(); // initialize brain screen
     chassis.calibrate(); // calibrate sensors
 
-    // turn on optical sensor LED so it can see color
-    optical.set_led_pwm(100); // 0–100% brightness
-    pros::delay(200);         // short delay for sensor to stabilize
-
     // the default rate is 50. however, if you need to change the rate, you
     // can do the following.
     // lemlib::bufferedStdout().setRate(...);
@@ -121,18 +111,6 @@ void initialize() {
             pros::lcd::print(0, "X: %f", chassis.getPose().x);      // x
             pros::lcd::print(1, "Y: %f", chassis.getPose().y);      // y
             pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
-
-            // read optical sensor
-            double hue = optical.get_hue();              // 0–360 degrees
-            int proximity = optical.get_proximity();     // larger = closer object
-
-            // simple blue detection (tune thresholds as needed)
-            bool isBlue = (proximity > 80) && (hue > 180.0 && hue < 260.0);
-
-            // show optical sensor debug + blue detection on brain screen
-            pros::lcd::print(3, "Hue: %.1f", hue);
-            pros::lcd::print(4, "Prox: %d", proximity);
-            pros::lcd::print(5, "Blue ball: %s", isBlue ? "YES" : "NO");
 
             // log position telemetry
             lemlib::telemetrySink()->info("Chassis pose: {}", chassis.getPose());
@@ -151,15 +129,18 @@ void disabled() {}
  * runs after initialize if the robot is connected to field control
  */
 void competition_initialize() {}
+
 /**
  * Runs during auto
  *
  * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
 void autonomous() {
-    chassis.setPose(-159.71, -29.446, 135);
-    // rotate 90 degrees to face the goal
-    chassis.turnToHeading(90, 4000);
+    // set position to x:0, y:0, heading:0
+    chassis.setPose(0,0,0);
+    // turn to face heading 90 with a very long timeout
+    chassis.turnToHeading(90, 400);
+
 }
 
 bool pistonAState = false;
@@ -192,8 +173,8 @@ void opcontrol() {
         if (std::abs(leftY)  < DEADBAND) leftY  = 0;
         if (std::abs(rightX) < DEADBAND) rightX = 0;
 
-        int throttle = leftY;     // forward/back
-        int turn     = -rightX;   // left/right turn
+        int throttle = -leftY;     // forward/back
+        int turn     = rightX;   // left/right turn
 
         chassis.curvature(throttle, turn);
 
